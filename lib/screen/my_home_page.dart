591581de5077470fc2887c8711cc21e0aa7ad45a
oraclelabpischool <oraclepischool@gmail.com>
2025-11-20 11:58:22 +0700

    feat: add to list example

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -9,6 +9,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List listStudent = [];
+  String classSelected = 'X';
+  String majorSelected = 'PPLG';
+  String genderSelected = 'Male';
+  TextEditingController fullName = TextEditingController();
+  TextEditingController age = TextEditingController();
+  final List<DropdownMenuEntry<String>> listClasses =
+      <DropdownMenuEntry<String>>[
+        const DropdownMenuEntry(value: "X", label: "X"),
+        const DropdownMenuEntry(value: "XI", label: "XI"),
+        const DropdownMenuEntry(value: "XII", label: "XII"),
+      ];
+  final List<DropdownMenuEntry<String>> listMajor = <DropdownMenuEntry<String>>[
+    const DropdownMenuEntry(value: "PPLG", label: "PPLG"),
+    const DropdownMenuEntry(value: "HR", label: "HR"),
+    const DropdownMenuEntry(value: "MPLB", label: "MPLB"),
+    const DropdownMenuEntry(value: "TM", label: "TM"),
+    const DropdownMenuEntry(value: "TSM", label: "TSM"),
+    const DropdownMenuEntry(value: "TKRO", label: "TKRO"),
+    const DropdownMenuEntry(value: "DKV", label: "DKV"),
+    const DropdownMenuEntry(value: "TJKT", label: "TJKT"),
+  ];
+  final List<DropdownMenuEntry<String>> genderList =
+      <DropdownMenuEntry<String>>[
+        const DropdownMenuEntry(value: "Male", label: "Male"),
+        const DropdownMenuEntry(value: "Female", label: "Female"),
+      ];
+
   void _showModalSheet() {
     showModalBottomSheet(
       context: context,
@@ -16,13 +44,92 @@ class _MyHomePageState extends State<MyHomePage> {
         return SizedBox(
           width: double.infinity,
           height: 400.0,
-          child: Column(
+          child: ListView(
             children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
                   "Add Student",
                   style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: fullName,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(labelText: "Full Name"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: age,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: "Age"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownMenu(
+                  width: double.infinity,
+                  dropdownMenuEntries: listClasses,
+                  initialSelection: classSelected,
+                  onSelected: (String? value) => setState(() {
+                    classSelected = value as String;
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownMenu(
+                  initialSelection: majorSelected,
+                  width: double.infinity,
+                  dropdownMenuEntries: listMajor,
+                  onSelected: (String? value) => setState(() {
+                    classSelected = value as String;
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownMenu(
+                  width: double.infinity,
+                  initialSelection: genderSelected,
+                  dropdownMenuEntries: genderList,
+                  onSelected: (String? value) => setState(() {
+                    genderSelected = value as String;
+                  }),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (fullName.text == "" || age.text == "") {
+                      Navigator.pop(context);
+                      const snackdemo = SnackBar(
+                        content: Text('Student name and age must be filled!'),
+                        backgroundColor: Colors.red,
+                        elevation: 10,
+                        behavior: SnackBarBehavior.floating,
+                        margin: EdgeInsets.all(5),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackdemo);
+                      return;
+                    }
+
+                    listStudent.add({
+                      "full_name": fullName.text,
+                      "classes": classSelected,
+                      "major": majorSelected,
+                      "Age": age.text,
+                      "gender": genderSelected,
+                    });
+                    Navigator.pop(context);
+                    print("BUtton On Click $listStudent");
+                  },
+                  child: const Text("Add Student"),
                 ),
               ),
             ],
@@ -52,7 +159,11 @@ class _MyHomePageState extends State<MyHomePage> {
         color: Colors.blue,
         child: Column(
           children: [
-            ContainerCustome(title1: "CONTENT 1", title2: "CONTENT 2"),
+            for (int idx = 0; idx < listStudent.length; idx++)
+              ContainerCustome(
+                title1: "Name: ${listStudent[idx]['full_name']}",
+                title2: "Class: ${listStudent[idx]['classes']}",
+              ),
           ],
         ),
       ),
